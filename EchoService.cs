43636c2c6ffe2329202b8f900a466468438d086d
qitsuk <kasper.andreasen@gmail.com>
2014-02-27 09:32:48 +0100

    Made changes

---
+++ EchoService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    class EchoService
+    {
+         private TcpClient connectionSocket;
+
+        public EchoService(TcpClient connectionSocket)
+        {
+            // TODO: Complete member initialization
+            this.connectionSocket = connectionSocket;
+        }
+        public void DoIt()
+        {
+            Stream ns = connectionSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = sr.ReadLine();
+            string answer;
+            while (message != null && message != "")
+            {
+                Console.WriteLine("Client: " + message);
+                answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+
+            }
+            connectionSocket.Close();
+        }
+
+        public string answer { get; set; }
+    }
+}
---
+++ SimpleHttpServer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    internal class SimpleHttpServer
+    {
+        public static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener serverSocket = new TcpListener(ip, 6789);
+
+            //TcpListener serverSocket = new TcpListener(6789);
+            serverSocket.Start();
+
+            while (true)
+            {
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Server activated now");
+                EchoService service = new EchoService(connectionSocket);
+                Thread myThread = new Thread(new ThreadStart(service.DoIt));
+                myThread.Start();
+            }
+            serverSocket.Stop();
+        }
+    }
+}
